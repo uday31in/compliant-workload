554f878b4a6376dfcdcc008043ac1606ead236fb
Marvin Buss <marvin.buss@gmail.com>
2023-03-05 20:15:47 +0100

    Add core terraform files

---
+++ code/terraform/variables.tf
@@ -0,0 +1,92 @@
+variable "location" {
+  description = "Specifies the location for all Azure resources."
+  type        = string
+  sensitive   = false
+}
+
+variable "environment" {
+  description = "Specifies the environment of the deployment."
+  type        = string
+  sensitive   = false
+  default     = "dev"
+  validation {
+    condition     = contains(["dev", "tst", "prd"], var.environment)
+    error_message = "Please use an allowed value: \"dev\", \"tst\" or \"prd\"."
+  }
+}
+
+variable "prefix" {
+  description = "Specifies the prefix for all resources created in this deployment."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(var.prefix) >= 2 && length(var.prefix) <= 10
+    error_message = "Please specify a prefix with more than two and less than 10 characters."
+  }
+}
+
+variable "tags" {
+  description = "Specifies the tags that you want to apply to all resources."
+  type        = map(any)
+  sensitive   = false
+}
+
+variable "subnet_id" {
+  description = "Specifies the resource ID of the subnet used for the deployment."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.subnet_id)) == 11
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "private_dns_zone_id_blob" {
+  description = "Specifies the resource ID of the private DNS zone for Azure Storage blob endpoints."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = var.private_dns_zone_id_blob == "" || (length(split("/", var.private_dns_zone_id_blob)) == 9 && endswith(var.private_dns_zone_id_blob, "privatelink.blob.core.windows.net"))
+    error_message = "Please specify a valid resource ID for the private DNS Zone."
+  }
+}
+
+variable "private_dns_zone_id_dfs" {
+  description = "Specifies the resource ID of the private DNS zone for Azure Storage dfs endpoints."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = var.private_dns_zone_id_dfs == "" || (length(split("/", var.private_dns_zone_id_dfs)) == 9 && endswith(var.private_dns_zone_id_dfs, "privatelink.dfs.core.windows.net"))
+    error_message = "Please specify a valid resource ID for the private DNS Zone."
+  }
+}
+
+variable "private_dns_zone_id_queue" {
+  description = "Specifies the resource ID of the private DNS zone for Azure Storage queue endpoints."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = var.private_dns_zone_id_queue == "" || (length(split("/", var.private_dns_zone_id_queue)) == 9 && endswith(var.private_dns_zone_id_queue, "privatelink.queue.core.windows.net"))
+    error_message = "Please specify a valid resource ID for the private DNS Zone."
+  }
+}
+
+variable "private_dns_zone_id_key_vault" {
+  description = "Specifies the resource ID of the private DNS zone for Azure Key Vault."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = var.private_dns_zone_id_key_vault == "" || (length(split("/", var.private_dns_zone_id_key_vault)) == 9 && endswith(var.private_dns_zone_id_key_vault, "privatelink.vaultcore.azure.net"))
+    error_message = "Please specify a valid resource ID for the private DNS Zone."
+  }
+}
+
+variable "ddos_protection_plan_id" {
+  description = "Specifies the resource ID of the DDoS protection plan."
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.ddos_protection_plan_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
