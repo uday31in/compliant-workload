9dc0a4a48b29011201e56782e4519643cacdf7a6
Marvin Buss <marvin.buss@gmail.com>
2023-03-06 11:02:49 +0100

    Update main terraform templates

--- code/terraform/variables.tf
+++ code/terraform/variables.tf
@@ -31,12 +31,32 @@ variable "tags" {
   sensitive   = false
 }
 
-variable "subnet_id" {
-  description = "Specifies the resource ID of the subnet used for the deployment."
+variable "vnet_id" {
+  description = "Specifies the resource ID of the Vnet used for the Data Landing Zone"
   type        = string
   sensitive   = false
   validation {
-    condition     = length(split("/", var.subnet_id)) == 11
+    condition     = length(split("/", var.vnet_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "nsg_id" {
+  description = "Specifies the resource ID of the default network security group for the Data Landing Zone"
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.nsg_id)) == 9
+    error_message = "Please specify a valid resource ID."
+  }
+}
+
+variable "route_table_id" {
+  description = "Specifies the resource ID of the default route table for the Data Landing Zone"
+  type        = string
+  sensitive   = false
+  validation {
+    condition     = length(split("/", var.route_table_id)) == 9
     error_message = "Please specify a valid resource ID."
   }
 }
