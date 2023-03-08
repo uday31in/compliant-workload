6b74cd583ee193ee1ca0066e3d58c49afe9822f0
Marvin Buss <marvin.buss@gmail.com>
2023-03-08 00:54:47 +0000

    Add private dns zones

--- code/terraform/modules/network/outputs.tf
+++ code/terraform/modules/network/outputs.tf
@@ -15,3 +15,27 @@ output "subnet_compute_id" {
   description = "Specifies the resource ID of the Compute subnet."
   sensitive   = false
 }
+
+output "private_dns_zone_blob_id" {
+  value       = azurerm_private_dns_zone.private_dns_zone_blob.id
+  description = "Specifies the resource ID of the Private DNS Zone."
+  sensitive   = false
+}
+
+output "private_dns_zone_key_vault_id" {
+  value       = azurerm_private_dns_zone.private_dns_zone_key_vault.id
+  description = "Specifies the resource ID of the Private DNS Zone."
+  sensitive   = false
+}
+
+output "private_dns_zone_cognitive_service_id" {
+  value       = azurerm_private_dns_zone.private_dns_zone_cognitive_service.id
+  description = "Specifies the resource ID of the Private DNS Zone."
+  sensitive   = false
+}
+
+output "private_dns_zone_open_ai_id" {
+  value       = azurerm_private_dns_zone.private_dns_zone_open_ai.id
+  description = "Specifies the resource ID of the Private DNS Zone."
+  sensitive   = false
+}
