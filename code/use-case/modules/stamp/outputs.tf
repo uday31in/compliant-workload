db5cbea733b703ff2768feb28e2141051868d100
Marvin Buss <marvin.buss@gmail.com>
2023-04-05 01:16:58 +0200

    fmt

--- code/use-case/modules/stamp/outputs.tf
+++ code/use-case/modules/stamp/outputs.tf
@@ -1,5 +1,5 @@
 output "open_ai_endpoint" {
-    value = azurerm_cognitive_account.cognitive_service.endpoint
-    description = "Specifies the endpoint of the Open AI Service."
-    sensitive = false
+  value       = azurerm_cognitive_account.cognitive_service.endpoint
+  description = "Specifies the endpoint of the Open AI Service."
+  sensitive   = false
 }
