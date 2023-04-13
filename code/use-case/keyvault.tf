92aa81865ca770295632aef468953e10c02861d2
Marvin Buss <marvin.buss@gmail.com>
2023-04-13 18:18:02 +0200

    lint

--- code/use-case/keyvault.tf
+++ code/use-case/keyvault.tf
@@ -27,9 +27,9 @@ data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_key_vault" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_key_vault" {
-  name                           = "logAnalytics"
-  target_resource_id             = azurerm_key_vault.key_vault.id
-  log_analytics_workspace_id     = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  name                       = "logAnalytics"
+  target_resource_id         = azurerm_key_vault.key_vault.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 
   dynamic "enabled_log" {
     iterator = entry
