95e7a7193f3fd985b3bfd8e3de6457b9ee273a80
Marvin Buss <marvin.buss@gmail.com>
2023-04-13 18:17:48 +0200

    Lint

--- code/use-case/logging.tf
+++ code/use-case/logging.tf
@@ -21,9 +21,9 @@ data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_application_
 }
 
 resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_application_insights" {
-  name                           = "logAnalytics"
-  target_resource_id             = azurerm_application_insights.application_insights.id
-  log_analytics_workspace_id     = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  name                       = "logAnalytics"
+  target_resource_id         = azurerm_application_insights.application_insights.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 
   dynamic "enabled_log" {
     iterator = entry
@@ -72,9 +72,9 @@ data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_log_analytic
 }
 
 resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_log_analytics_workspace" {
-  name                           = "logAnalytics"
-  target_resource_id             = azurerm_log_analytics_workspace.log_analytics_workspace.id
-  log_analytics_workspace_id     = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  name                       = "logAnalytics"
+  target_resource_id         = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 
   dynamic "enabled_log" {
     iterator = entry
