7c4a431d0b9f2cf66caedadddc51370dfecdd786
Marvin Buss <marvin.buss@gmail.com>
2023-04-20 21:25:11 +0200

    Update logic app resources

--- code/use-case-logic/logicapp.tf
+++ code/use-case-logic/logicapp.tf
@@ -1,28 +1,19 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "service_plan" {
   name                = "${local.prefix}-asp001"
   location            = var.location
   resource_group_name = azurerm_resource_group.app_rg.name
   tags                = var.tags
 
-  is_xenon                     = false
-  kind                         = "elastic"
   maximum_elastic_worker_count = 20
-  per_site_scaling             = false
-  reserved                     = false
-  sku {
-    tier     = "WorkflowStandard"
-    size     = "WS1"
-    capacity = 3
-  }
-  zone_redundant = true
-}
-
-resource "azapi_resource" "logic_app_standard" {
-  
+  os_type                      = "Windows"
+  per_site_scaling_enabled     = false
+  sku_name                     = "WS1"
+  worker_count                 = 3
+  zone_balancing_enabled       = true
 }
 
 resource "azurerm_logic_app_standard" "logic_app_standard" {
-  name = "${local.prefix}-logic001"
+  name                = "${local.prefix}-logic001"
   location            = var.location
   resource_group_name = azurerm_resource_group.app_rg.name
   tags                = var.tags
@@ -33,34 +24,34 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
     ]
   }
 
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-  app_settings = {}
-  bundle_version = "[1.*, 2.0.0)"
+  app_service_plan_id     = azurerm_service_plan.service_plan.id
+  app_settings            = {}
+  bundle_version          = "[1.*, 2.0.0)"
   client_affinity_enabled = false
   client_certificate_mode = "Optional"
-  enabled = true
-  https_only = true
+  enabled                 = true
+  https_only              = true
   site_config {
-    always_on = false
-    app_scale_limit = 0
+    always_on                = false
+    app_scale_limit          = 0
     elastic_instance_minimum = 1
-    ftps_state = "FtpsOnly"
-    http2_enabled = false
-    min_tls_version = "1.2"
+    ftps_state               = "FtpsOnly"
+    http2_enabled            = false
+    min_tls_version          = "1.2"
     # pre_warmed_instance_count = 0
     runtime_scale_monitoring_enabled = false
     # scm_ip_restriction = []
-    scm_min_tls_version = "1.2"
-    scm_type = "None"
+    scm_min_tls_version         = "1.2"
+    scm_type                    = "None"
     scm_use_main_ip_restriction = true
-    use_32_bit_worker_process = true
-    vnet_route_all_enabled = true
-    websockets_enabled = false
+    use_32_bit_worker_process   = true
+    vnet_route_all_enabled      = true
+    websockets_enabled          = false
   }
-  storage_account_name = azurerm_storage_account.storage.name
+  storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  storage_account_share_name = 
-  use_extension_bundle = true
-  version = "~3"
-  virtual_network_subnet_id = azapi_resource.subnet_logicapp.id
+  storage_account_share_name = azapi_resource.storage_file_share.name
+  use_extension_bundle       = true
+  version                    = "~3"
+  virtual_network_subnet_id  = azapi_resource.subnet_logicapp.id
 }
