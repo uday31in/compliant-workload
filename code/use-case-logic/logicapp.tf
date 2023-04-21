61ec469a15410a56150e373c78cc24680bc94a8e
Marvin Buss <marvin.buss@gmail.com>
2023-04-21 12:06:49 +0200

    Update logic app and add diagnostics

--- code/use-case-logic/logicapp.tf
+++ code/use-case-logic/logicapp.tf
@@ -24,8 +24,15 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
     ]
   }
 
-  app_service_plan_id     = azurerm_service_plan.service_plan.id
-  app_settings            = {}
+  app_service_plan_id = azurerm_service_plan.service_plan.id
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.application_insights.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.application_insights.connection_string
+    "FUNCTIONS_WORKER_RUNTIME"              = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION"          = "~18"
+    "WEBSITE_CONTENTOVERVNET" = "1"
+    "WEBSITE_VNET_ROUTE_ALL"  = "1"
+  }
   bundle_version          = "[1.*, 2.0.0)"
   client_affinity_enabled = false
   client_certificate_mode = "Optional"
@@ -34,16 +41,27 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
   site_config {
     always_on                = false
     app_scale_limit          = 0
-    elastic_instance_minimum = 1
+    elastic_instance_minimum = 3
     ftps_state               = "FtpsOnly"
     http2_enabled            = false
+    ip_restriction = [
+      {
+        name = "AllowSentinel"
+        action = "Allow"
+        headers = null
+        ip_address = null
+        priority = 100
+        service_tag = "AzureSentinel"
+        virtual_network_subnet_id = null
+      }
+    ]
     min_tls_version          = "1.2"
-    # pre_warmed_instance_count = 0
+    # pre_warmed_instance_count = 0  # Uncomment to configure pre-warmed instance count
     runtime_scale_monitoring_enabled = false
-    # scm_ip_restriction = []
+    # scm_ip_restriction = []  # Uncomment to define IP restrictions for SCM endpoint
     scm_min_tls_version         = "1.2"
     scm_type                    = "None"
-    scm_use_main_ip_restriction = true
+    scm_use_main_ip_restriction = false
     use_32_bit_worker_process   = true
     vnet_route_all_enabled      = true
     websockets_enabled          = false
@@ -52,6 +70,76 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   storage_account_share_name = azapi_resource.storage_file_share.name
   use_extension_bundle       = true
-  version                    = "~3"
+  version                    = "~4"
   virtual_network_subnet_id  = azapi_resource.subnet_logicapp.id
+}
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_service_plan" {
+  resource_id = azurerm_service_plan.service_plan.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_service_plan" {
+  name                       = "logAnalytics"
+  target_resource_id         = azurerm_service_plan.service_plan.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_service_plan.log_category_groups
+    content {
+      category_group = entry.value
+      retention_policy {
+        enabled = true
+        days    = 30
+      }
+    }
+  }
+
+  dynamic "metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_service_plan.metrics
+    content {
+      category = entry.value
+      enabled  = true
+      retention_policy {
+        enabled = true
+        days    = 30
+      }
+    }
+  }
+}
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_logic_app_standard" {
+  resource_id = azurerm_logic_app_standard.logic_app_standard.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_logic_app_standard" {
+  name                       = "logAnalytics"
+  target_resource_id         = azurerm_logic_app_standard.logic_app_standard.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+
+  dynamic "enabled_log" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_logic_app_standard.log_category_groups
+    content {
+      category_group = entry.value
+      retention_policy {
+        enabled = true
+        days    = 30
+      }
+    }
+  }
+
+  dynamic "metric" {
+    iterator = entry
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories_logic_app_standard.metrics
+    content {
+      category = entry.value
+      enabled  = true
+      retention_policy {
+        enabled = true
+        days    = 30
+      }
+    }
+  }
 }
